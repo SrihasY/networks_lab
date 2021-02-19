4f0cdd0f17114efb9453dd72fe038649b2be0914
SrihasY <srihas65@gmail.com>
2021-02-19 11:21:19 +0530

    Edited client to send filename buffer-sized chunks.

--- ass6/file_client.c
+++ ass6/file_client.c
@@ -68,7 +68,8 @@ int main(void) {
     int cur_sent;
     //send repeatedly until the full filename is transferred
     while(file_sent!=end) {
-        if((cur_sent=send(cli_sockfd, file_sent, end-file_sent, 0))==-1) {
+        //send the filename in chunks of upto BUFSIZE
+        if((cur_sent=send(cli_sockfd, file_sent, BUFSIZE, 0))==-1) {
             printf("\nThe file request could not be sent. Error:%d. Exiting...\n", errno);
             close(cli_sockfd);
 		    exit(1);
