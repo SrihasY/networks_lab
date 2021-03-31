112aa807b5554f77e9a0330491af8a7f74b75815
somnathjena2011 <somnathjena.2011@gmail.com>
2021-03-31 14:38:26 +0530

    not tested

---
+++ ass8/chat.h
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include <inttypes.h>
+
+#define MAX_PEERS 5
+#define MAX_NAME 256
+
+struct user_entry {
+    char username[MAX_NAME];
+    char ip[16];
+    uint16_t port;
+    int cli_sockfd;
+};
+
+struct user_info {
+    struct user_entry table[MAX_PEERS];
+    int number_peers;
+};
